76b499d2858e0cb39b025c5f407b8223986ac4e7
Sheryl Chee <sherylmichaela@gmail.com>
2024-08-02 16:27:27 +1000

    Created Subtasks class with get method

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from config import app, api
 from flask import make_response, session, request
-from models import db, User, Task
+from models import db, User, Task, Subtask
 from flask_restful import Resource
 from datetime import datetime
 from sqlalchemy import and_
@@ -102,7 +102,7 @@ class TaskById(Resource):
         if task:
             return make_response(task.to_dict(), 200)
         
-        return make_response({"error": "You're not allowed to view this task"}, 401)
+        return make_response({"error": "This task doesn't exist or you may not have permission to view this task"}, 401)
 
     def patch(self, id):
         task = TaskById.find_task(id)
@@ -123,36 +123,105 @@ class TaskById(Resource):
         task = TaskById.find_task(id)
 
         if task:
-            db.session.delete()
+            db.session.delete(task)
             db.session.commit()
 
             return make_response({"message": "Task is deleted successfully"}, 200)
         
         return make_response({"error": "No task found"}, 404)
 
+class Subtasks(Resource):
+    # /tasks/<int:task_id>/subtasks
+
+    def get(self, task_id):
+
+        if 'user_id' in session:
+            task = Task.query.filter(Task.id == task_id, Task.user_id == session['user_id']).first()
+
+            if task:
+                my_subtasks = Subtask.query.filter(Subtask.task_id == task_id).all()
+
+                if len(my_subtasks) > 0:
+                    subtasks = [subtask.to_dict() for subtask in my_subtasks]
+                    return make_response(subtasks, 200)
+             
+            return make_response({"message": "No subtask created yet"}, 200)
+        
+        return make_response({"error": "Pls log in to view subtasks."}, 401)
     
+    # def post(self):
+    #     task_name = request.json.get('task_name')
+    #     category = request.json.get('category')
+    #     task_due_date = datetime.strptime(request.json.get('task_due_date'), '%Y-%m-%d')
+    #     task_status = request.json.get('task_status', 'pending')
 
-# class Tags(Resource):
-#     def get(self):
+    #     new_task = Task(task_name=task_name, category=category, task_due_date=task_due_date, task_status=task_status, user_id=session['user_id'])
 
-#         if 'user_id' in session:
-#             my_tasks = Task.query.filter(Task.user_id == session['user_id']).all()
+    #     db.session.add(new_task)
+    #     db.session.commit()
 
-#             if len(my_tasks) > 0:
-#                 tasks = [task.to_dict() for task in my_tasks]
-#                 return make_response(tasks, 200)
-                
-#             return make_response({"message": "No task created yet"}, 200)
+    #     if new_task.id:
+    #         return make_response(new_task.to_dict(), 201)
+        
+    #     return make_response({"error": "error occurred"}, 400)
 
-#         return make_response({"error": "Pls log in to view tasks."}, 401)
+# class SubtaskById(Resource):
+#     # /tasks/<int:id>/subtasks/<int:id>
+
+#     @classmethod
+#     def find_subtask(cls, task_id, subtask_id):
+#         return Subtask.query.filter(and_(Subtask.id == subtask_id, Subtask.task_id == task_id, Subtask.task.has(Task.user_id == session['user_id']))).first()
+    
+#     def get(self, task_id, subtask_id):
+#         subtask = SubtaskById.find_subtask(task_id, subtask_id)
+
+#         if subtask:
+#             return make_response(subtask.to_dict(), 200)
+
+#         return make_response({"error": "This subtask doesn't exist or you may not have permission to view this subtask"}, 401)
+
+#     def post(self, task_id):
+#         task = TaskById.find_task(task_id)
+
+#         if not task:
+#             return make_response({"error": "This task doesn't exist or you may not have permission to view this task"}, 401)
+        
+#         subtask_name = request.json.get('subtask_name')
+#         subtask_due_date = datetime.strptime(request.json.get('subtask_due_date'), '%Y-%m-%d')
+#         subtask_status = request.json.get('subtask_status', 'pending')
+
+#         subtask = Subtask(subtask_name=subtask_name, task_id=task_id, subtask_due_date=subtask_due_date, subtask_status=subtask_status)
+
+#         db.session.add(subtask)
+#         db.session.commit()
+
+#         return make_response(subtask.to_dict(), 201)
+
+# class AddTags(Resource):
+#     # /tasks/<int:id>/addtags
+
+#     @classmethod
+#     def find_task(cls, id):
+#         task = Task.query.filter(Task.id == id).first()
+
+#         if not task:
+#             return make_response({"error": "Task not found"}, 404)
+
+#     def get(self, id):
+#         tags = AddTags.find_task(id)
+        
 
 api.add_resource(Signup, '/signup', endpoint="signup")
 api.add_resource(Login, '/login', endpoint="login")
 api.add_resource(Logout, '/logout', endpoint="logout")
 api.add_resource(CheckSession, '/me', endpoint="me")
 
-api.add_resource(Tasks, '/tasks', endpoint='/tasks')
-api.add_resource(TaskById, '/tasks/<int:id>')
+api.add_resource(Tasks, '/tasks', endpoint='tasks')
+# api.add_resource(TaskById, '/tasks/<int:id>')
+
+api.add_resource(Subtasks, '/tasks/<int:task_id>/subtasks')
+
+# api.add_resource(SubtaskById, '/tasks/<int:task_id>/subtasks/<int:subtask_id>')
 
 if __name__ == "__main__":
     app.run(port=4000, debug=True)
